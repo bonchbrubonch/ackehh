e246abdc20cdd80ebc7e7bf9978052de21f7e546
Artem <nikolloteslla@gmail.com>
2023-05-30 19:48:33 +0300

    add remarks, pages blog s article

--- app/js/app.js
+++ app/js/app.js
@@ -19,21 +19,31 @@ $(function () {
     nextArrow: '<button class="slider__right"><svg width="18" height="8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.354 4.354a.5.5 0 000-.708L14.172.464a.5.5 0 10-.708.708L16.293 4l-2.829 2.828a.5.5 0 10.708.708l3.182-3.182zM0 4.5h17v-1H0v1z" fill="#fff"/></svg></button>',
   });
 
-  $('.documentation-slider').slick({
+
+  $('.documentation-slider__top').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    swipe: false,
+    asNavFor: '.documentation-slider__bottom',
+  });
+  $('.documentation-slider__bottom').slick({
     infinite: false,
     slidesToShow: 3,
     slidesToScroll: 1,
     variableWidth: true,
+    asNavFor: '.documentation-slider__top',
     prevArrow: '<button class="slider__left"><svg width="18" height="8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.354 4.354a.5.5 0 000-.708L14.172.464a.5.5 0 10-.708.708L16.293 4l-2.829 2.828a.5.5 0 10.708.708l3.182-3.182zM0 4.5h17v-1H0v1z" fill="#fff"/></svg></button>',
     nextArrow: '<button class="slider__right"><svg width="18" height="8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.354 4.354a.5.5 0 000-.708L14.172.464a.5.5 0 10-.708.708L16.293 4l-2.829 2.828a.5.5 0 10.708.708l3.182-3.182zM0 4.5h17v-1H0v1z" fill="#fff"/></svg></button>',
   });
 
 
 
-  $('.service__inner').each(function() {
+  $('.service__inner').each(function () {
     let ths = $(this);
     ths.find('.service__item').not(':first').hide();
-    ths.find('.service__btn').click(function() {
+    ths.find('.service__btn').click(function () {
       ths.find('.service__btn').removeClass('active').eq($(this).index()).addClass('active');
       ths.find('.service__item').hide().eq($(this).index()).fadeIn()
     }).eq(0).addClass('active');
@@ -47,5 +57,8 @@ $(function () {
     readOnly: true
   });
 
-
+  if ($('.slick-slider').length > 0) {
+    $(".slick-slider").slick("refresh");
+    $(".slick-slider").slick("setPosition");
+  }
 });
